59f800430c438ae605f42657a5a6eb825f8fde23
maonyoro <maonyoro@gmail.com>
2017-06-18 16:25:03 +0900

    update sync

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 BUCKET="cruel-reason.net"
 PUBLIC_DIR="public"
 
-if [ `grep AKIAIYWZJCF ~/.aws/credentials  | wc -l` -eq 1 ]; then
+if [ `grep IYWZJCF ~/.aws/credentials  | wc -l` -eq 1 ]; then
   aws s3 sync ${PUBLIC_DIR} s3://${BUCKET}
 else
   echo "confirm your aws credentials."
