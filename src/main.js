72cc9a74f462342878ff6b7bfe72f991f15828f1
mao <maonyoro@gmail.com>
2017-07-29 15:25:50 +0900

    add libraries

--- src/main.js
+++ src/main.js
@@ -5,8 +5,13 @@ import App from './App'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'vue-awesome/icons'
+import Icon from 'vue-awesome/components/Icon'
+import VueLightbox from 'vue-lightbox'
 
 Vue.use(BootstrapVue)
+Vue.component('icon', Icon)
+Vue.component('Lightbox', VueLightbox)
 
 Vue.config.productionTip = false
 
