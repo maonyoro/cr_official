7915b3acceb2052a867cf0bfdeaa950589e4dddb
mao <maonyoro@gmail.com>
2017-07-29 23:23:55 +0900

    ディレクトリ構成変更

---
+++ new/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+BUCKET="new-cruel-reason"
+PUBLIC_DIR="dist"
+
+if [ `grep IYWZJCF ~/.aws/credentials  | wc -l` -eq 1 ]; then
+  aws s3 sync ${PUBLIC_DIR} s3://${BUCKET} --delete
+else
+  echo "confirm your aws credentials."
+fi
--- new/src/main.js
+++ new/src/main.js
@@ -1,7 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -9,6 +8,7 @@ import 'vue-awesome/icons'
 import Icon from 'vue-awesome/components/Icon'
 import VueLightbox from 'vue-lightbox'
 import Slick from 'vue-slick'
+import App from './App'
 
 Vue.use(BootstrapVue)
 Vue.component('icon', Icon)
