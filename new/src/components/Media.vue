3646af2a679476e67fc7b06dad260f13657db067
mao <maonyoro@gmail.com>
2017-12-03 00:29:35 +0900

    add 3rd mini

--- new/src/components/Media.vue
+++ new/src/components/Media.vue
@@ -18,6 +18,9 @@
     <div class="row mt-3" v-show="isMovieSelected()">
       <div class="col-12">
         <!-- 2nd-->
+        <h5 class="text-left">3rdミニアルバム「Advancement...」プレビュー</h5>
+        <iframe width="320" height="320" src="https://www.youtube.com/embed/1nFqU-kmrEI" frameborder="0" allowfullscreen class="w-100 mb-5"></iframe>
+        <!-- 2nd-->
         <h5 class="text-left">2ndミニアルバム「狂深層目録」プレビュー</h5>
         <iframe width="320" height="320" src="https://www.youtube.com/embed/sZ21CWxQEQY" frameborder="0" allowfullscreen class="w-100 mb-5"></iframe>
         <!-- 1st-->
@@ -28,6 +31,42 @@
 
     <!-- Discography -->
     <div class="mt-4" v-show="isDiscoSelected()">
+
+      <div id="3rd" class="mb-4">
+        <div class="row">
+          <div class="col-12 text-left mb-2">
+            <h4 class="border-bottom">3rd Mini Album 「Advancement...」</h4>
+          </div>
+        </div>
+        <div class="row text-left">
+          <div class="col-5">
+            <lightbox :src="require('../assets/jacket/3rd_jacket.png')">
+              <img src="../assets/jacket/3rd_jacket.png" class="w-100">
+            </lightbox>
+            <ol class="ml-4 mt-3">
+              <li>泡</li>
+              <li>Break from the Past</li>
+              <li>Claustrophobia</li>
+              <li>沫</li>
+              <li>Cry of minority</li>
+              <li>étoile</li>
+            </ol>
+          </div>
+          <div class="col-7">
+            <iframe scrolling="no" id="hearthis_at_user_81344-5015992" width="100%" height="350" src="https://hearthis.at/set/81344-5015992/embed/" frameborder="0" allowtransparency></iframe>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12 text-left">
+            <ul>
+              <li>商品番号 AZMF-3110</li>
+              <li>価格 2160円(税込)</li>
+              <li>発売元/販売元 有限会社ジェナイトミュージックファクトリー</li>
+            </ul>
+            <p>ライブ会場物販にて販売中</p>
+          </div>
+        </div>
+      </div>
 
       <div id="2nd" class="mb-4">
         <div class="row">
