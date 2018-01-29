b6e71e4b2e25006bc94021fc56cb8e05e8596e31
maonyoro <maonyoro@gmail.com>
2018-01-29 23:01:58 +0900

    update schedule

--- new/src/components/Live.vue
+++ new/src/components/Live.vue
@@ -49,34 +49,14 @@ export default {
     return {
       liveData: [
         {
-          date: '1/6(土)',
-          place: '浦和ナルシス',
-          title: '～漆黒のシンフォニー～2018',
-          open: '17:00',
-          start: '17:30',
-          charge: { rsv: '3200', adv: '3700', drink: '500' },
-          bands: ['CRUEL REASON', 'Geomatrium', 'マーブル', '白夢', '勇†気セッション'],
-          option: ''
-        },
-        {
-          date: '1/20(土)',
-          place: '池袋CYBER',
-          title: 'LuLu主催 『隆佑医師BD記念祭 Ryu-suke Rock Festival 2018～Rebirth～』',
-          open: '16:00',
-          start: '16:30',
-          charge: { rsv: '3500', adv: '4000', drink: '600' },
-          bands: ['CRUEL REASON', 'LuLu', 'La\'veil MizeriA', 'G.S.I', '変態集団AnimA', 'CRISIS', 'SARRMATH', 'LOVE TRAP'],
-          option: '<p>【チケット発売中】<br>[A] 池袋CYBER店頭販売(A1～A50 シャッフル、A51～整理番号順) ※1人3枚まで / [B] バンド予約</p>'
-        },
-        {
           date: '2/11(日)',
           place: '浦和ナルシス',
           title: 'Unlimited colors～CRUEL REASON yuri Birthday～',
-          open: '16:30',
-          start: '17:00',
+          open: '17:00',
+          start: '17:30',
           charge: { rsv: '3000', adv: '3500', drink: '500' },
-          bands: ['CRUEL REASON', 'and more...'],
-          option: ''
+          bands: ['CRUEL REASON', 'ugly betty\'s paranoia', 'RF', '眼鏡ギタァ', '柏-変態集団AnimA-'],
+          option: '<p>チケット引き換え 16:30~</p>'
         },
         {
           date: '3/10(土)',
