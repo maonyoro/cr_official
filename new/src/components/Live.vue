f2b64b875a3835d0fc303c4d61e2ae9aa2012652
maonyoro <maonyoro@gmail.com>
2018-02-28 00:22:58 +0900

    update schedule

--- new/src/components/Live.vue
+++ new/src/components/Live.vue
@@ -34,7 +34,7 @@
             <li v-for="b in l.bands" :key="b">{{b}}</li>
           </ul>
           <div v-if="!!l.option" class="mb-1">
-            <span v-html="l.option"></span>
+            <p v-html="l.option"></p>
           </div>
         </div>
       </div>
@@ -49,23 +49,73 @@ export default {
     return {
       liveData: [
         {
-          date: '2/11(日)',
+          date: '3/10(土)',
           place: '浦和ナルシス',
-          title: 'Unlimited colors～CRUEL REASON yuri Birthday～',
+          title: 'Unlimited colors ～CRUEL REASON 英人 Birthday～',
           open: '17:00',
           start: '17:30',
           charge: { rsv: '3000', adv: '3500', drink: '500' },
-          bands: ['CRUEL REASON', 'ugly betty\'s paranoia', 'RF', '眼鏡ギタァ', '柏-変態集団AnimA-'],
-          option: '<p>チケット引き換え 16:30~</p>'
+          bands: [
+            'CRUEL REASON',
+            'DANGER☆GANG',
+            'Abyss',
+            'Black Bullet',
+            '月影先生(from Heretique Aventure)',
+            'シークレットバンド'
+          ],
+          option: ''
         },
         {
-          date: '3/10(土)',
+          date: '3/31(土) ※maoセッション「浦和鋼鉄同好会」出演',
           place: '浦和ナルシス',
-          title: '英人Birthday',
+          title: '桜華爛漫',
+          open: '17:00',
+          start: '17:30',
+          charge: { rsv: '3000', adv: '3500', drink: '500' },
+          bands: [
+            '浦和鋼鉄同好会 [Vo.Kiri／Gt.mao／Gt.GENTARO(ReWilD)／Ba.悠(Geomatrium)／Dr.樹]',
+            'ROMiO.',
+            'コインロッカーベイビー(ズ)',
+            'プラチナ',
+            '変態集団AnimA'
+          ],
+          option: '※CRUEL REASONの出演はございません'
+        },
+        {
+          date: '4/15(日)',
+          place: '浦和ナルシス',
+          title: '未定',
           open: '未定',
           start: '未定',
           charge: { rsv: '-', adv: '-', drink: '500' },
           bands: ['CRUEL REASON', 'and more...'],
+          option: ''
+        },
+        {
+          date: '5/4(祝)',
+          place: '浦和ナルシス',
+          title: 'mao Birthday live',
+          open: '未定',
+          start: '未定',
+          charge: { rsv: '-', adv: '-', drink: '500' },
+          bands: ['CRUEL REASON', 'and more...'],
+          option: ''
+        },
+        {
+          date: '6/3(日)',
+          place: '浦和ナルシス',
+          title: 'Zёnit Music Factory PRESENTS『ジェナイト祭り』',
+          open: '未定',
+          start: '未定',
+          charge: { rsv: '-', adv: '-', drink: '500' },
+          bands: [
+            'CRUEL REASON',
+            'DANGER☆GANG',
+            '[enver]brain',
+            'Geomatrium',
+            'RF',
+            'ROMiO.'
+          ],
           option: ''
         }
       ]
