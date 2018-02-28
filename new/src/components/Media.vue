e20fb251048d6c690c6ad63e54ea94186429bf8d
maonyoro <maonyoro@gmail.com>
2018-02-28 23:32:42 +0900

    add bandcamp

--- new/src/components/Media.vue
+++ new/src/components/Media.vue
@@ -52,11 +52,8 @@
               <li>étoile</li>
             </ol>
           </div>
-          <!--div class="col-7">
-            <iframe scrolling="no" id="hearthis_at_user_81344-5015992" width="100%" height="350" src="https://hearthis.at/set/81344-5015992/embed/" frameborder="0" allowtransparency></iframe>
-          </div-->
           <div class="col-7">
-            <iframe style="border: 0; width: 400px; height: 340px;" src="https://bandcamp.com/EmbeddedPlayer/album=616200324/size=large/bgcol=333333/linkcol=e32c14/artwork=small/transparent=true/" seamless><a href="http://cruel-reason.bandcamp.com/album/advancement">Advancement… by CRUEL REASON</a></iframe>
+            <iframe style="border: 0; width: 100%; height: 340px;" src="https://bandcamp.com/EmbeddedPlayer/album=616200324/size=large/bgcol=333333/linkcol=e99708/artwork=none/transparent=true/" seamless><a href="http://cruel-reason.bandcamp.com/album/advancement">Advancement… by CRUEL REASON</a></iframe>
           </div>
         </div>
         <div class="row">
@@ -82,9 +79,7 @@
             <lightbox :src="require('../assets/jacket/2nd_jacket.png')">
               <img src="../assets/jacket/2nd_jacket_s.png" class="w-100">
             </lightbox>
-          </div>
-          <div class="col-7">
-            <ol>
+            <ol class="ml-4 mt-3">
               <li>虚絶</li>
               <li>心傷</li>
               <li>Algorithmic Society</li>
@@ -92,10 +87,12 @@
               <li>幻添雨</li>
             </ol>
           </div>
+          <div class="col-7">
+            <iframe style="border: 0; width: 100%; height: 307px;" src="https://bandcamp.com/EmbeddedPlayer/album=1346049904/size=large/bgcol=333333/linkcol=e32c14/artwork=none/transparent=true/" seamless><a href="http://cruel-reason.bandcamp.com/album/-">狂深層目録 by CRUEL REASON</a></iframe>
+          </div>
         </div>
         <div class="row">
           <div class="col-12 text-left">
-            <a href="https://hearthis.at/cruel-reason/set/63253-5015992/">試聴(外部リンク)</a>
             <ul>
               <li>商品番号 AZMF-3105</li>
               <li>価格 2160円(税込)</li>
@@ -105,7 +102,7 @@
           </div>
         </div>
       </div>
-  
+
       <div id="1st">
         <div class="row">
           <div class="col-12 text-left mb-2">
@@ -117,9 +114,7 @@
             <lightbox :src="require('../assets/jacket/mugen_jacket.png')">
               <img src="../assets/jacket/mugen_jacket_s.png" class="w-100">
             </lightbox>
-          </div>
-          <div class="col-7">
-            <ol>
+            <ol class="ml-4 mt-3">
               <li>∞</li>
               <li>Gimlet</li>
               <li>the season</li>
@@ -127,10 +122,12 @@
               <li>COLOR</li>
             </ol>
           </div>
+          <div class="col-7">
+            <iframe style="border: 0; width: 100%; height: 307px;" src="https://bandcamp.com/EmbeddedPlayer/album=3530875583/size=large/bgcol=ffffff/linkcol=63b2cc/artwork=none/transparent=true/" seamless><a href="http://cruel-reason.bandcamp.com/album/--2">夢幻泡影 by CRUEL REASON</a></iframe>
+          </div>
         </div>
         <div class="row">
           <div class="col-12 text-left">
-            <a href="https://hearthis.at/cruel-reason/set/52282-5015992/" target="_blank">試聴(外部リンク)</a>
             <ul style="list-style:none">
               <li>商品番号 AZMF-3097</li>
               <li>価格 2160円(税込)</li>
